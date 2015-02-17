a1b058363720d6fe47b7a297cac57fffb31be2b5
Connor K <connor.kuehnle@gmail.com>
2015-02-16 23:06:27 -0800

    Initial Commit

---
+++ TileGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TileGame
+//
+//  Created by Connor Kuehnle on 2/16/15.
+//  Copyright (c) 2015 Connor Kuehnle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
